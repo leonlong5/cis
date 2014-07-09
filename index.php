2bff9ed181503edc1f61843bff198d7e736e9e3e
unknown <long.wang56@gmail.com>
2014-07-09 14:29:09 -0400

    add cis

---
+++ index.php
@@ -0,0 +1,69 @@
+<!Doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>CIS FIU</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="frequent asked question">
+    <meta name="author" content="Long Wang">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/media_query_index.css">
+  </head>
+
+  <body>
+    <?php include_once("nav.php");?>
+      <div class="wrap">      
+        <div class="content">
+          <div class="main col">
+            FIU CIS Graduate Program
+            <p>
+              The School of Computing and Information Sciences (SCIS) offers both a Master of Science degree and a Doctor of Philosophy degree. The Master of Science degree provides study in state-of-the-art computer applications as well as an introduction to the theoretical foundations of computer science. The Doctor of Philosophy in Computer Science is designed to provide study in all major areas of computer science while leading to the frontiers of knowledge in a chosen field of concentration.
+            </p>
+          </div>
+          
+          <div class="secondary col">
+            Secondary
+            <p>
+              Describing words describe something. They modify something. Adjectives are one type of describing word that are commonly used. They are used to describe a noun or a pronoun. Adverbs are second type of descriptive word. They are used to describe a verb or an adverb. The pages on this site are organized by a word’s first letter. This is just demo for displaying the layout.
+            </p>
+          </div>
+          
+          <div class="extra col">
+            Introduction
+            <p>
+              Our faculty embodies a broad range of computing interests and has built dynamic and innovative research centers and groups around several areas of specialization. These include:
+            </p>
+            <p>Database Systems, including database design, database management and applications, database theory and implementation, database machines, distributed databases, information retrieval in heterogeneous databases, multimedia databases, data mining and digital libraries.
+            </p>
+            <p>
+              Software Engineering, including large-scale software design, programming language environments, software development and maintenance methodologies, object-oriented techniques, software reuse, and software quality assurance.
+            </p>
+            <p>
+              Parallel and Distributed Systems, including formal specification methodologies, distributed file systems, distributed multimedia systems and operating systems.
+            </p>
+            <p>
+              Computer Networks, including network protocols, multimedia networking, and wireless communications.
+            </p>
+            <p>
+              Theory, including algorithms and data structures, programming languages, computer security, program verification and logic.
+              Artificial Intelligence, including neural networks, expert systems, automated reasoning, term rewriting systems and intelligent tutoring systems.
+            </p>
+            <p>
+              Cognitive Science, with emphasis on the philosophical, psychological and linguistic underpinnings of artificial intelligence.
+              Bioinformatics and Computational Biology.
+            </p>
+          </div>
+          <br style="clear:both">
+        </div>
+      </div>
+
+      <footer class="main-footer">
+        <div class="fwrapper">
+          <div class="fcol">link</div>
+          <div class="fcol">contact</div>
+          <div class="fcol">information</div>
+        </div>
+      </fotter>
+    </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,10 @@
+      <header class="main-header">
+          <a class="logo" href="#"><h1>FIU CIS</h1></a>
+          <nav class="main-nav">
+            <a href="http://matrix.cs.fiu.edu/cis/index.php">Home</a>
+            <a href="http://matrix.cs.fiu.edu/cis/faq.php">FAQ</a>
+            <a href="#">Graduate Seminar</a>
+            <a href="#">Graduate students</a>
+            <a href="#">Contact</a>
+          </nav>
+      </header>
