c3338969d680023a9368af009cf0392e4335794e
unknown <long.wang56@gmail.com>
2014-07-10 17:32:00 -0400

    add xeditable to faq

--- faq.php
+++ faq.php
@@ -1,5 +1,5 @@
 <!Doctype html>
-<html>
+<html ng-app="app">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -10,6 +10,9 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/style_faq.css">
     <link rel="stylesheet" type="text/css" href="css/media_query_faq.css">
+    <link href="css/xeditable.css" rel="stylesheet">
+
+
   </head>
 
   <body>
@@ -18,7 +21,7 @@
       <div class="wrap">      
         <div class="content">
           <div class="main col">
-            <h2>Frequent Asked Questions</h2>
+            <h2>Summary</h2>
             <p>
               At here you can find the most frequently asked questions by students, check it when you have questions. We will keep updating the questions, if you have any question or suggessions, please contact us!
             </p>
@@ -27,30 +30,13 @@
           
           
           <div class="extra col">
-            <h2>Introduction</h2>
-            <p>
-              Our faculty embodies a broad range of computing interests and has built dynamic and innovative research centers and groups around several areas of specialization. These include:
-            </p>
-            <p>Database Systems, including database design, database management and applications, database theory and implementation, database machines, distributed databases, information retrieval in heterogeneous databases, multimedia databases, data mining and digital libraries.
-            </p>
-            <p>
-              Software Engineering, including large-scale software design, programming language environments, software development and maintenance methodologies, object-oriented techniques, software reuse, and software quality assurance.
-            </p>
-            <p>
-              Parallel and Distributed Systems, including formal specification methodologies, distributed file systems, distributed multimedia systems and operating systems.
-            </p>
-            <p>
-              Computer Networks, including network protocols, multimedia networking, and wireless communications.
-            </p>
-            <p>
-              Theory, including algorithms and data structures, programming languages, computer security, program verification and logic.
-              Artificial Intelligence, including neural networks, expert systems, automated reasoning, term rewriting systems and intelligent tutoring systems.
-            </p>
-            <p>
-              Cognitive Science, with emphasis on the philosophical, psychological and linguistic underpinnings of artificial intelligence.
-              Bioinformatics and Computational Biology.
-            </p>
-            <button class="btn"><a href="">Click Me</a></button>
+            <h2>Frequent Asked Questions</h2>
+            <div ng-controller="TextareaCtrl">
+              <a href="#" editable-textarea="user.desc" e-rows="7" e-cols="40">
+                <pre style="color:white;">{{ user.desc || 'no description' }}</pre>
+              </a>
+            </div>
+            
           </div>
 
           
@@ -59,5 +45,18 @@
       </div>
 
 <?php include_once("footer.php");?>
+    
+<!-- put js at the end of the document -->
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
+<script src="js/xeditable.js"></script>
+
+<script type="text/javascript">
+        var app = angular.module("app", ["xeditable"]);
+        app.controller('TextareaCtrl', function($scope) {
+          $scope.user = {
+            desc: 'Title \ndescription!'
+          };
+        });
+</script>
     </body>
 </html>
