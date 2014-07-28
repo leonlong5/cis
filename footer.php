e5a9dc88806f6b409033e318d4fe20a66f2b5fc6
unknown <long.wang56@gmail.com>
2014-07-28 16:04:15 -0400

    seminar added

--- footer.php
+++ footer.php
@@ -2,12 +2,19 @@
       <footer class="main-footer">
         <div class="fwrapper">
           <div class="fcol"><h2>Link</h2>
-            <p><a href="http://www.cis.fiu.edu/">cis home</a></p>
+            <p><a href="http://www.cis.fiu.edu/">FIU CIS home</a></p>
             <p><a href="http://www.cis.fiu.edu/advising/">Advising</a></p>
+            <p><a href="http://www.cis.fiu.edu/support">Support & Services</a></p>
           </div>
           <div class="fcol"><h2>Contact</h2>
-            <p>Phone:</p>
-            <p>Email:</p>
+            <p>Main Office： 305.348.2744
+              Fax: 305.348.3549</p>
+            <p>Address:</p>
+            <p>Florida International University school of Computing and Information Sciences
+            11200 SW 8th St 
+            ECS 354 
+            Miami, FL 33199
+            </p>
           </div>
           <div class="fcol"><h2>Administrator</h2>
             <?php
--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
           <nav class="main-nav">
             <a href="http://matrix.cs.fiu.edu/cis/index.php">Home</a>
             <a href="http://matrix.cs.fiu.edu/cis/faq.php">FAQ</a>
-            <a href="#">Graduate Seminar</a>
+            <a href="http://matrix.cs.fiu.edu/cis/grad_seminar.php">Graduate Seminar</a>
             <a href="http://matrix.cs.fiu.edu/cis/grad_stud.php">Graduate students</a>
           </nav>
       </header>
